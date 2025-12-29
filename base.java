e2a6f505c9bdb423ecb6bab6cb2485ea21392086
rinki <rinkimittal20062GAMIL.COM>
2025-12-29 12:59:56 +0530

    initial commit

---
+++ base.java
@@ -0,0 +1,6 @@
+import java.lang.*;
+class base{
+public static void main(String[] args){
+System.out.println("this is base class");
+}
+}
---
+++ max2.java
@@ -0,0 +1,5 @@
+class max2{
+public static void main(String[] args){
+System.out.println(args[1]);
+}
+}
